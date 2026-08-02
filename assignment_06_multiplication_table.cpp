0aedeef706831d5bea45c304313a1abb017f3a28
Yajagee Nakoja Samuel <nsyagee002@st.ug.edu.gh>
2026-08-02 14:18:16 +0000

    Complete all assignments

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -56,4 +56,44 @@
 
 #include <iostream>
 using namespace std;
+void printSingleTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
 
+void printTablesUpToN(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        cout << "Multiplication Table for " << num << ":" << endl;
+        for (int i = 1; i <= 12; i++) {
+            cout << num << " x " << i << " = " << (num * i) << endl;
+        }
+        if (num < n) {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    // ---------- PART A ----------
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printSingleTable(number);
+
+    cout << endl;
+
+    // ---------- PART B ----------
+    int n;
+    cout << "Enter a number N: ";
+    cin >> n;
+    printTablesUpToN(n);
+
+    return 0;
+}
